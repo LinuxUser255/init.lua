548d62c1244ecfade2c5d020fcf134448dc22bae
Chris <atnl@protonmail.com>
2023-06-29 11:09:46 -0400

    Update packer.lua

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -15,8 +15,13 @@ return require('packer').startup(function(use)
   	requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use 'folke/tokyonight.nvim'
-
+    use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
 
   -- All other plugins
   use({
@@ -31,8 +36,14 @@ return require('packer').startup(function(use)
       end
   })
 
+  use {
+			'nvim-treesitter/nvim-treesitter',
+			run = function()
+				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+				ts_update()
+			end,}
 
-  use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+  -- use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
   use("nvim-treesitter/playground")
   use("theprimeagen/harpoon")
   use("theprimeagen/refactoring.nvim")
