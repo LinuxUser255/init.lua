21fea0cd731ee7a9ba8c92cef8e0a40823703295
Chris <atnl@protonmail.com>
2023-06-29 11:10:10 -0400

    Update packer.lua

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -1,6 +1,5 @@
 --This file can be loaded by calling `lua require('plugins')` from your init.vim
 
-  --use 'folke/tokyonight.nvim'
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
