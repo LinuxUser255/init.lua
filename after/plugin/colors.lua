57b708c4c9f89706c81aa9566c7d3c67c68bc0d2
Chris <atnl@protonmail.com>
2023-07-06 12:17:21 -0400

    Update colors.lua

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,16 +1,14 @@
--- color scheme and transparent background
- --require("tokyonight.colors").setup({
---     disable_background = false
- --})
+require('rose-pine').setup({
+    disable_background = true
+})
 
- function ColorMyPencils(color)
-	color = color or "tokyonight"
+function ColorMyPencils(color) 
+	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-    -- uncomment the following lines to enable transparent background.
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
